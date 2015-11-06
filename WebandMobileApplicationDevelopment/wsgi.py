24db5425469af969fb0de512e44fc6d648898b5a
Aditya Gupta <contactadityag@gmail.com>
2015-11-06 02:08:20 -0500

    Initial commit

---
+++ WebandMobileApplicationDevelopment/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for WebandMobileApplicationDevelopment project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebandMobileApplicationDevelopment.settings")
+
+application = get_wsgi_application()

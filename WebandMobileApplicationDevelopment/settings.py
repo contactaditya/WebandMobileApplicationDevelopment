9de0d256cab443b8d0455e88dfaff3427e46eaa3
Aditya Gupta <contactadityag@gmail.com>
2015-12-07 19:02:56 -0500

    Just Updating The Front End!!

--- WebandMobileApplicationDevelopment/settings.py
+++ WebandMobileApplicationDevelopment/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         # The following settings are not used with sqlite3
         'USER': 'root',
 
-        'PASSWORD': '',
+        'PASSWORD': '1234',
         # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP
         'HOST': '127.0.0.1',
         # Set to empty string for default
